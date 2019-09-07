16a6bc116c1c130261e165fb284e615e4d1a98d8
vivekbhr <bhardwaj@ie-freiburg.mpg.de>
2019-09-07 21:11:57 +0200

    dont re-install

--- rules/velocyto_indexing.R
+++ rules/velocyto_indexing.R
@@ -14,10 +14,11 @@ if (!require(BiocManager)) {
   install.packages("BiocManager")
 }
 # Install from GitHub
-BiocManager::install(c("BSgenome", "GenomicFeatures", "AnnotationDbi", "AnnotationHub", "pcaMethods"))
-devtools::install_github("BUStools/BUSpaRse")
-devtools::install_github("satijalab/seurat-wrappers")
-devtools::install_github("velocyto-team/velocyto.R")
+#BiocManager::install(c("BSgenome", "GenomicFeatures", "AnnotationDbi", "AnnotationHub", "pcaMethods"))
+
+if (!require(BUSpaRse)) devtools::install_github("BUStools/BUSpaRse")
+if (!require(seurat-wrappers)) devtools::install_github("satijalab/seurat-wrappers")
+if (!require(seurat-wrappers)) devtools::install_github("velocyto-team/velocyto.R")
 
 
 ## load appropriate bsgenome
